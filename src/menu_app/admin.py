eeac83a595ac6cf062c6119237885b5d0826d3dd
Sergey <ssnovak@stud.kpfu.ru>
2024-02-18 15:14:06 +0300

    without the possibility of specifying recursion

--- src/menu_app/admin.py
+++ src/menu_app/admin.py
@@ -7,3 +7,11 @@ from .models import MenuItem
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ['title', 'parent']
     list_filter = ['parent']
+    search_fields = ('title', 'parent__title')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "parent":
+            obj_id = request.resolver_match.kwargs.get('object_id')
+            if obj_id:
+                kwargs["queryset"] = self.model.objects.exclude(id=obj_id)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
